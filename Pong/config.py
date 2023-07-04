f96841ba9c760f5e3dc7706ca5ecd6da78b83a7f
abreugui-maker <51962675+abreugui-maker@users.noreply.github.com>
2023-07-04 13:43:33 -0300

    add file via upload
    The pong game working. But there isn't any sprite  or game menu

---
+++ Pong/config.py
@@ -0,0 +1,13 @@
+class Config():
+    # FPS
+    FPS = 10
+    FPS_GAMING = 100
+    # Define screen Size
+    SCREENSIZE = (640, 400)
+    # Colors
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+    BLUE = (0, 0, 255)
+    # Title
+    TITLE = "Pong"
